3c5d8b92ca1d13b8a4e676a51d8a063a762b5cd6
fieme-one <Fieme1145@gmail.com>
2025-08-20 11:17:23 +0300

    update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
  import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/supabase.min.js';
 
     const supabaseUrl = 'https://ilsddenybmvxvzvouwls.supabase.co';
-    const supabaseKey = 'YOUR_SUPABASE_KEY_HERE';
+    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlsc2RkZW55Ym12eHZ6dm91d2xzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU0MjA2MDcsImV4cCI6MjA3MDk5NjYwN30.6VkZpSNO7bzv1pbDOTUC8yPompIUCMmfqQ0laRN1JoU';
     const supabase = createClient(supabaseUrl, supabaseKey);
 
     const emailInput = document.getElementById('email');
