b20ab5eeac4176292da23fb61d94c29625113be0
fieme-one <Fieme1145@gmail.com>
2025-11-26 10:39:26 +0300

    update popShower.js

---
+++ popShower.js
@@ -0,0 +1,26 @@
+export async function popLOGIN(param) {
+  var pop = document.getElementById("popUpLogin")
+  if(!pop){
+    pop = document.createElement('div')
+    pop.className = "popLOGIN"
+    pop.id="popUpLogin"
+    pop.style=`justify-content: center;
+  color: #888;
+  position: fixed;
+  top: 30px;
+  justify-self: center;
+  background: white;
+  width: 70%;
+  border: 1px solid black;
+  border-radius: 10px;
+  padding: 10px;
+  animation: slow 0.5s ease-in-out;display: flex;
+`
+document.body.appendChild(pop)
+  }
+  pop.innerHTML = `<span>login in...</span>`
+}
+export function stopAllPop(){
+  var pops = document.querySelector(".popLOGIN")
+  pops.remove()
+}
